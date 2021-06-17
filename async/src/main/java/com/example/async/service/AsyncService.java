0a91eb9d677f31289f94eb62d2e76671db8fd8a0
KimJaeHyun <kjhkjh5265@smail.kongju.ac.kr>
2021-06-17 10:33:34 +0900

    '210617'

---
+++ async/src/main/java/com/example/async/service/AsyncService.java
@@ -0,0 +1,16 @@
+package com.example.async.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Service
+public class AsyncService {
+
+    public CompletableFuture hello(){
+
+    }
+}
